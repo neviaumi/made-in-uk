2be458fba4e450c3364d2e0236cf41a209b9d5bd
neviaumi <david.ng.dev@gmail.com>
2024-02-25 20:59:19 +0000

    GH-16: enable deployment on github
    this close #16

--- systems/infrastructure/scripts/ci/deploy.sh
+++ systems/infrastructure/scripts/ci/deploy.sh
@@ -4,14 +4,13 @@ set -ex
 PROJECT=made-in-uk
 STACK=$1
 
-
-
 npm run build
 STATE_STORE_BUCKET=$(node ./bin/setup-state-store.js made-in-uk-iac-state-store)
 pulumi login "gs://$STATE_STORE_BUCKET"
-
 set +e
 PULUMI_CONFIG_PASSPHRASE= pulumi stack init organization/$PROJECT/$STACK
 set -e
+PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:project made-in-uk
+PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:region europe-west2
 PULUMI_CONFIG_PASSPHRASE= pulumi stack select organization/$PROJECT/$STACK
 PULUMI_CONFIG_PASSPHRASE= pulumi up --yes
--- systems/infrastructure/scripts/dev-deploy.sh
+++ systems/infrastructure/scripts/dev-deploy.sh
@@ -11,9 +11,9 @@ STATE_STORE_BUCKET=$(node ./bin/setup-state-store.js made-in-uk-iac-state-store)
 pulumi login "gs://$STATE_STORE_BUCKET"
 set +e
 PULUMI_CONFIG_PASSPHRASE= pulumi stack init organization/$PROJECT/$STACK
+set -e
 PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:project made-in-uk
 PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:region europe-west2
-set -e
 PULUMI_CONFIG_PASSPHRASE= pulumi stack select organization/$PROJECT/$STACK
 PULUMI_CONFIG_PASSPHRASE= pulumi up --yes
 PULUMI_CONFIG_PASSPHRASE= pulumi stack output --shell > "$WORK_SPACE_ROOT"/.env
