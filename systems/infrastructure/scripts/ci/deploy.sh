3bdf061db96f38756f26ecf28fb4b29ebcae0b8b
neviaumi <david.ng.dev@gmail.com>
2024-02-25 20:59:19 +0000

    GH-16: enable deployment on github
    this close #16

--- systems/infrastructure/scripts/ci/deploy.sh
+++ systems/infrastructure/scripts/ci/deploy.sh
@@ -9,9 +9,11 @@ STACK=$1
 npm run build
 STATE_STORE_BUCKET=$(node ./bin/setup-state-store.js made-in-uk-iac-state-store)
 pulumi login "gs://$STATE_STORE_BUCKET"
-
+pulumi config set gcp:region <value>
 set +e
 PULUMI_CONFIG_PASSPHRASE= pulumi stack init organization/$PROJECT/$STACK
 set -e
+PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:project made-in-uk
+PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:region europe-west2
 PULUMI_CONFIG_PASSPHRASE= pulumi stack select organization/$PROJECT/$STACK
 PULUMI_CONFIG_PASSPHRASE= pulumi up --yes
