171fc2ae593f0e11e35a7641fe735e76ae7925a3
neviaumi <david.ng.dev@gmail.com>
2024-02-25 20:59:19 +0000

    GH-16: enable deployment on github
    this close #16

--- systems/infrastructure/scripts/ci/deploy.sh
+++ systems/infrastructure/scripts/ci/deploy.sh
@@ -4,14 +4,18 @@ set -ex
 PROJECT=made-in-uk
 STACK=$1
 
-
-
 npm run build
 STATE_STORE_BUCKET=$(node ./bin/setup-state-store.js made-in-uk-iac-state-store)
 pulumi login "gs://$STATE_STORE_BUCKET"
-
 set +e
 PULUMI_CONFIG_PASSPHRASE= pulumi stack init organization/$PROJECT/$STACK
 set -e
 PULUMI_CONFIG_PASSPHRASE= pulumi stack select organization/$PROJECT/$STACK
+PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:credentials $GOOGLE_APPLICATION_CREDENTIALS
+PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:project made-in-uk
+PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:region europe-west2
+echo $GOOGLE_APPLICATION_CREDENTIALS
+#gcloud auth login --cred-file=$GOOGLE_APPLICATION_CREDENTIALS
+#gcloud config list
+gcloud auth configure-docker europe-west2-docker.pkg.dev
 PULUMI_CONFIG_PASSPHRASE= pulumi up --yes
