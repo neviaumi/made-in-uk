d63bfb201c13d7305342449321d76ce4d9fe5952
neviaumi <david.ng.dev@gmail.com>
2024-02-25 20:59:19 +0000

    GH-16: enable deployment on github
    this close #16

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,15 @@ set -ex
 ENVIRONMENT=$1
 
 if [ -z "$CI" ]; then
-  # https://github.com/orgs/community/discussions/26560
+  echo ""
+else
+  echo "I am in CI"
   git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
   git config user.name "github-actions[bot]"
 fi
 
+echo "Running in email: $(git config user.email)"
+echo "Running in name: $(git config user.name)"
 # Disable the commit hook
 export HUSKY=0
 SCRIPT_LOCATION=$(dirname $(pwd)/${BASH_SOURCE[0]})
@@ -25,11 +29,10 @@ fi
 export RELEASE_BRANCH="release-$RELEASE_VERSION"
 export RELEASE_VERSION=$RELEASE_VERSION
 export CURRENT_BRANCH=$CURRENT_BRANCH
-COMMIT_MESSAGE="release v$RELEASE_VERSION [skip ci]"
+COMMIT_MESSAGE="release candidate v$RELEASE_VERSION [skip ci]"
 git switch -c "$RELEASE_BRANCH"
 git push --set-upstream origin "$RELEASE_BRANCH"
 npx lerna version --message "$COMMIT_MESSAGE" --yes $RELEASE_VERSION
-#npx lerna version --no-git-tag-version --no-push --yes $RELEASE_VERSION
 npx lerna exec --stream \
 --scope 'infrastructure' \
 -- "bash scripts/ci/deploy.sh $ENVIRONMENT"
@@ -40,16 +43,20 @@ npx lerna exec --stream \
 
 source $WORK_SPACE_ROOT/.env
 
-export API_CLOUD_RUN_SERVICE_NAME=$API_CLOUD_RUN_SERVICE_NAME
-export API_DATABASE_ID=$API_DATABASE_ID
-export DOCKER_REGISTRY=$DOCKER_REGISTRY
-export WEB_API_HOST=$WEB_API_HOST
-export WEB_CLOUD_RUN_SERVICE_NAME=$WEB_CLOUD_RUN_SERVICE_NAME
+#export API_CLOUD_RUN_SERVICE_NAME=$API_CLOUD_RUN_SERVICE_NAME
+#export API_DATABASE_ID=$API_DATABASE_ID
+#export DOCKER_REGISTRY=$DOCKER_REGISTRY
+#export WEB_API_HOST=$WEB_API_HOST
+#export WEB_CLOUD_RUN_SERVICE_NAME=$WEB_CLOUD_RUN_SERVICE_NAME
+#
+#npx lerna exec --stream \
+#--scope 'api' \
+#-- "bash scripts/ci/deploy.sh"
+#
+#npx lerna exec --stream \
+#--scope 'web' \
+#-- "bash scripts/ci/deploy.sh"
 
-npx lerna exec --stream \
---scope 'api' \
--- "bash scripts/ci/deploy.sh"
-
-npx lerna exec --stream \
---scope 'web' \
--- "bash scripts/ci/deploy.sh"
+git add .
+git commit -m "release v$RELEASE_VERSION [skip ci]"
+git push
