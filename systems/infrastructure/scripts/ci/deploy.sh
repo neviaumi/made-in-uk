58bffa7f04679ec4354c90beaf63579f2c7d67bd
neviaumi <david.ng.dev@gmail.com>
2024-02-27 17:14:18 +0000

    fixup! fixup! fixup! GH-16: enable deployment on github

--- systems/infrastructure/scripts/ci/deploy.sh
+++ systems/infrastructure/scripts/ci/deploy.sh
@@ -15,9 +15,10 @@ PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:credentials $GOOGLE_APPLICATION_
 PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:project made-in-uk
 PULUMI_CONFIG_PASSPHRASE= pulumi config set gcp:region europe-west2
 #echo $GOOGLE_APPLICATION_CREDENTIALS
-#echo $GCP_SA_KEY > $CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE
+echo $GCP_SA_KEY > $CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE
 #echo $CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE
 #gcloud auth login --cred-file=$GOOGLE_APPLICATION_CREDENTIALS
 #gcloud config list
 gcloud auth configure-docker europe-west2-docker.pkg.dev
+gcloud auth application-default print-access-token
 PULUMI_CONFIG_PASSPHRASE= pulumi up --yes
