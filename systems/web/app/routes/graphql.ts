302803ebc846a83f5fd627ad1ed3530b51611a08
David Ng <david.ng.dev@gmail.com>
2024-05-24 23:49:26 +0100

    GH-211: include more debug log
    this fix #211

--- systems/web/app/routes/graphql.ts
+++ systems/web/app/routes/graphql.ts
@@ -3,6 +3,12 @@ import { type ActionFunctionArgs } from '@remix-run/node';
 import { createFetchClient } from '../fetch.server.ts';
 
 export async function action({ request }: ActionFunctionArgs) {
+  // eslint-disable-next-line no-console
+  console.log({
+    body: request.body,
+    headers: Object.fromEntries(request.headers.entries()),
+    message: '/graphql',
+  });
   const fetchClient = createFetchClient();
   const gqlResponse = await fetchClient('/graphql', {
     body: request.body,
