327fefc8fce7327423f85135dee8117d9822319c
David Ng <david.ng.dev@gmail.com>
2024-05-24 22:02:15 +0100

    GH-211: fix attempt
    this fix #211

--- systems/web/app/routes/graphql.ts
+++ systems/web/app/routes/graphql.ts
@@ -3,10 +3,10 @@ import { type ActionFunctionArgs } from '@remix-run/node';
 import { createFetchClient } from '../fetch.server.ts';
 
 export async function action({ request }: ActionFunctionArgs) {
-  const fetchClient = createFetchClient('');
+  const fetchClient = createFetchClient();
   const gqlResponse = await fetchClient('/graphql', {
     body: request.body,
-    headers: request.headers,
+    headers: Object.fromEntries(request.headers.entries()),
     method: 'POST',
   });
 
