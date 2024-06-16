ac6723917c6ae6bb933fce54ebd3b8b6c883a006
David Ng <david.ng.dev@gmail.com>
2024-06-16 22:09:54 +0100

    GH-282: display error message for debug

--- systems/web/app/routes/graphql.ts
+++ systems/web/app/routes/graphql.ts
@@ -3,10 +3,6 @@ import { type ActionFunctionArgs } from '@remix-run/node';
 import { createFetchClient } from '../fetch.server.ts';
 
 export async function action({ request }: ActionFunctionArgs) {
-  // eslint-disable-next-line no-console
-  console.log({
-    message: 'proxy of /graphql',
-  });
   const fetchClient = createFetchClient();
   const gqlResponse = await fetchClient('/graphql', {
     body: request.body,
