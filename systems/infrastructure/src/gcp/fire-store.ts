7a5bda3c41d3cac4de41ef1973bdd9e80ca814aa
David Ng <david.ng.dev@gmail.com>
2024-06-18 11:59:21 +0100

    GH-290: iac to set up cloud tasks

--- systems/infrastructure/src/gcp/fire-store.ts
+++ systems/infrastructure/src/gcp/fire-store.ts
@@ -1,11 +1,11 @@
 import { firestore } from '@pulumi/gcp';
 
-import { getGcpConfig } from '../utils/get-gcp-config.ts';
+import { getLocation } from '../utils/get-gcp-config.ts';
 import { resourceName } from '../utils/resourceName.ts';
 
 export function createFireStoreDB() {
   const dbRef = new firestore.Database(resourceName`db`, {
-    locationId: getGcpConfig(),
+    locationId: getLocation(),
     type: 'FIRESTORE_NATIVE',
   });
   return {
