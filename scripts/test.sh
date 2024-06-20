5678d44e2cc6a04328a4a64e44f85d4f80243190
David Ng <david.ng.dev@gmail.com>
2024-06-20 03:28:26 +0100

    GH-318: update dev config
    this part of GH-318

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,9 @@ set -ex
 CURRENT_BRANCH=$(git branch --show-current)
 echo "Current branch is $CURRENT_BRANCH"
 npx eslint .
-
-npx lerna exec --stream \
--- "test ! -f  scripts/ci/test.sh || bash \
-                                scripts/ci/test.sh"
+if [ ! -z "$CI" ]
+then
+  npx lerna exec --stream \
+  -- "test ! -f  scripts/ci/test.sh || bash \
+                                  scripts/ci/test.sh"
+fi
