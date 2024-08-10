5c9f94db1e09cd8c9ef1c9fdda0bd1a8429de95e
David Ng <david.ng.dev@gmail.com>
2024-08-10 23:51:59 +0100

    GH-340: remove gcloud storage caching
    this close #340

--- systems/llm/scripts/ci/deploy.sh
+++ systems/llm/scripts/ci/deploy.sh
@@ -5,6 +5,8 @@ SCRIPT_LOCATION=$(dirname $(pwd)/${BASH_SOURCE[0]})
 APP_ROOT=$(realpath "$SCRIPT_LOCATION"/../../)
 DOCKER_REGISTRY=${DOCKER_REGISTRY}
 
+pdm run python ./src/llm/pre_load_modal.py
+
 tag=$(cat $APP_ROOT/package.json | jq -r '.version')
 docker build -t "${DOCKER_REGISTRY}/llm:$tag" -f ./Dockerfile .
 docker push "$DOCKER_REGISTRY/llm:$tag"
--- systems/llm/src/llm/pre_load_modal.py
+++ systems/llm/src/llm/pre_load_modal.py
@@ -1,11 +1,3 @@
 import llm_engine
-import os
-from google.cloud import storage
-bucket_name = os.environ['LLM_STORAGE_BUCKET']
-storage_client = storage.Client()
-bucket = storage_client.bucket(bucket_name)
-blob = bucket.blob(llm_engine.model_name)
-if not blob.exists():
-    model_saved = llm_engine.save_locally()
-    blob.upload_from_filename(model_saved)
-blob.download_to_filename(llm_engine.cache_dir.joinpath(llm_engine.model_name))
+
+llm_engine.save_locally()
