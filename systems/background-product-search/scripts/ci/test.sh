9647f88d72709483dd53d6609151a6070ef5cecd
David Ng <david.ng.dev@gmail.com>
2024-08-16 15:46:05 +0100

    GH-352: fix the failed test
    this fix #352

--- systems/background-product-search/scripts/ci/test.sh
+++ systems/background-product-search/scripts/ci/test.sh
@@ -4,4 +4,9 @@ set -ex
 npx vite build
 npx eslint -c eslint.config.js .
 npx tsc
+export FIRESTORE_EMULATOR_HOST=unused
+export BG_PRODUCT_SEARCH_PRODUCT_DETAIL_LOW_PRIORITY_QUEUE=unused
+export BG_PRODUCT_SEARCH_PRODUCT_DETAIL_QUEUE=unused
+export CLOUD_TASKS_EMULATOR_HOST=unused
+export BG_PRODUCT_SEARCH_PRODUCT_DETAIL_ENDPOINT=unused
 BG_PRODUCT_SEARCH_ENV=test npx vitest run --no-file-parallelism --passWithNoTests
