643c11613750e3a58d1adc4ed108ac9d489a756d
David Ng <david.ng.dev@gmail.com>
2024-09-14 00:16:09 +0100

    GH-400: replace build-in http server with fastify
    this close #400

--- systems/background-product-search/src/logger.ts
+++ systems/background-product-search/src/logger.ts
@@ -1,6 +1,8 @@
+import { type FastifyBaseLogger } from 'fastify';
 import {
   createLogger as createWinstonLogger,
   format,
+  type Logger,
   transports,
 } from 'winston';
 
@@ -9,7 +11,27 @@ import { Level, levels } from '@/logger.types.ts';
 
 const config = loadConfig(APP_ENV);
 
-export type { Logger } from 'winston';
+type LoggerPatched = FastifyBaseLogger | Logger;
+
+export type { LoggerPatched as Logger };
+
+export function adaptToFastifyLogger(logger: Logger): FastifyBaseLogger {
+  // see https://fastify.dev/docs/latest/Reference/Server/#logger
+  return {
+    child: (...args: Parameters<Logger['child']>) => {
+      return adaptToFastifyLogger(logger.child(...args));
+    },
+    debug: logger.debug.bind(logger),
+    error: logger.error.bind(logger),
+    fatal: logger.error.bind(logger),
+    info: logger.info.bind(logger),
+    level: logger.level,
+    silent: () => {},
+    trace: logger.debug.bind(logger),
+    warn: logger.warning.bind(logger),
+  };
+}
+
 export function createLogger(appEnv: AppEnvironment) {
   const isDev = [AppEnvironment.DEV].includes(appEnv);
   return createWinstonLogger({
