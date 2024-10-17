307f91b4d4a099767177fcaca1f843b5de3502c5
David Ng <david.ng.dev@gmail.com>
2024-10-17 01:14:55 +0100

    GH-472: fix the migration of vite compiler
    this part of #472

--- systems/web/app/error.server.ts
+++ systems/web/app/error.server.ts
@@ -3,12 +3,3 @@ import { isNativeError as _isNativeError } from 'node:util/types';
 export function isNativeError(e: unknown): e is NodeJS.ErrnoException {
   return _isNativeError(e);
 }
-
-export function withErrorCode(
-  code: 'ERR_UNAUTHENTICATED' | 'ERR_UNEXPECTED_ERROR' | 'ERR_REVOKED_SESSION',
-) {
-  return (e: NodeJS.ErrnoException) => {
-    e.code = code;
-    return e;
-  };
-}
