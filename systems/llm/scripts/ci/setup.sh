17d03f3c02b8b84943e76e55c98060116c3ecd49
David Ng <david.ng.dev@gmail.com>
2024-10-31 02:32:54 +0000

    GH-461: exclude llama.cpp group from ci
    this part of #461

--- systems/llm/scripts/ci/setup.sh
+++ systems/llm/scripts/ci/setup.sh
@@ -2,4 +2,4 @@
 
 set -e
 pip install -U pdm
-PDM_CHECK_UPDATE=false pdm install --check --no-editable --frozen-lockfile
+PDM_CHECK_UPDATE=false pdm install --check --no-editable --frozen-lockfile --without llama.cpp
