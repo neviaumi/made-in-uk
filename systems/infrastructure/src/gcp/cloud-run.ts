0c05df625e0c2f1d7ae966c66e87d8e44571fac3
David Ng <david.ng.dev@gmail.com>
2024-10-31 15:32:15 +0000

    GH-461: increase memory on llm instance to solve oom
    this part of #461

--- systems/infrastructure/src/gcp/cloud-run.ts
+++ systems/infrastructure/src/gcp/cloud-run.ts
@@ -400,7 +400,7 @@ export function createCloudRunForLLM({
             resources: {
               limits: {
                 cpu: '8',
-                memory: '4096Mi',
+                memory: '8192Mi',
               },
             },
             startupProbe: {
